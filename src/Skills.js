15b10c8e62ec07722629e5c44941abee307572a1
Christian Florez <florez.christian@gmail.com>
2017-07-29 12:06:57 -0400

    add skills section

--- src/Skills.js
+++ src/Skills.js
@@ -8,9 +8,47 @@ const Skills = () => {
         <Row>
           <Col md={12} xs={10} xsOffset={1}>
             <h2 id="skills">Skills</h2>
-            <Col md={4} mdOffset={2} xs={10} xsOffset={1}>
+            <Col md={4} xs={10} xsOffset={1} mdOffset={0}>
+              <h3>Front-End:</h3>
+              <ul>
+                <li>React</li>
+                <li>Redux</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SASS</li>
+                <li>JavaScript (ES6 and ES7)</li>
+                <li>Bootstrap</li>
+                <li>jQuery</li>
+                <li>webpack</li>
+                <li>Chrome DevTools</li>
+              </ul>
             </Col>
-            <Col md={4} xs={10} xsOffset={1}>
+            <Col md={4} xs={10} xsOffset={1} mdOffset={0}>
+              <h3>Back-End:</h3>
+              <ul>
+                <li>Node</li>
+                <li>Express</li>
+                <li>SQL</li>
+                <li>MongoDB</li>
+                <li>PostgreSQL</li>
+                <li>Redis</li>
+                <li>WebSockets</li>
+                <li>Amazon S3</li>
+                <li>OAuth</li>
+                <li>Session-Based Authentication</li>
+                <li>Testing with Jasmine</li>
+              </ul>
+            </Col>
+            <Col md={4} xs={10} xsOffset={1} mdOffset={0}>
+              <h3>Methodologies/Other:</h3>
+              <ul>
+                <li>Version Control with Git</li>
+                <li>Heroku Deployment</li>
+                <li>Entity Relationship Modeling</li>
+                <li>Test Driven Development</li>
+                <li>RESTful API Design</li>
+                <li>Agile Development</li>
+              </ul>
             </Col>
           </Col>
         </Row>
