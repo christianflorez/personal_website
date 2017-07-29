6a26d795c8bb98da3836cd34ee20853ca298c1db
Christian Florez <florez.christian@gmail.com>
2017-07-29 00:47:26 -0400

    add basic navbar

---
+++ src/Navigation.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+
+const NavLinks = () => {
+  return (
+    <Navbar.Collapse>
+      <Nav pullRight>
+          <NavItem eventKey={1} href="#portfolio">Portfolio</NavItem>
+          <NavItem eventKey={2} href="#skills">Skills</NavItem>
+          <NavItem eventKey={3} href="#about">About</NavItem>
+          <NavItem eventKey={4} href="#contact">Contact</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  );
+};
+
+const Navigation = ({ title }) => {
+  return (
+    <Navbar fluid>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a>{title}</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <NavLinks />
+    </Navbar>
+  );
+};
+
+export default Navigation;
